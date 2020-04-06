5e39f8f42aa66bed34402c6d552b86af50815f2a
Saqib <saqibnoorani@gmail.com>
2020-04-06 08:22:00 +0530

    Modified with api structure change

--- src/api.ts
+++ src/api.ts
@@ -1,45 +1,48 @@
-import * as https from 'https'
+import * as https from 'https';
 import * as axios from 'axios';
 import to from 'await-to-js';
-var url = "coronavirus-19-api.herokuapp.com";
-var indiaURl = 'https://api.covid19india.org/data.json'
-
+var url = 'coronavirus-19-api.herokuapp.com';
+var indiaURl = 'https://api.covid19india.org/data.json';
 
 function getLatestData(callback: (arg0: any) => void) {
-    return https.get({
-        host: url,
-        path: '/all'
-    }, function (response: any) {
-        var body = '';
-        response.on('data', (value: any) => {
-            body += value;
-        });
-        response.on('end', () => {
-            callback(JSON.parse(body));
-        });
-    });
+  return https.get(
+    {
+      host: url,
+      path: '/all',
+    },
+    function (response: any) {
+      var body = '';
+      response.on('data', (value: any) => {
+        body += value;
+      });
+      response.on('end', () => {
+        callback(JSON.parse(body));
+      });
+    }
+  );
 }
 
-function getCountryWiseData(param: string,callback: (arg0: any) => void) {
-    return https.get({
-        host: url,
-        path: "/countries/"+ param
-    }, function (response: any) {
-        var body = '';
-        response.on('data', (value: any) => {
-            body += value;
-        });
-        response.on('end', () => {
-            callback(JSON.parse(body));
-        });
-    });
+function getCountryWiseData(param: string, callback: (arg0: any) => void) {
+  return https.get(
+    {
+      host: url,
+      path: '/countries/' + param,
+    },
+    function (response: any) {
+      var body = '';
+      response.on('data', (value: any) => {
+        body += value;
+      });
+      response.on('end', () => {
+        callback(JSON.parse(body));
+      });
+    }
+  );
 }
 
-async function getDetailsForIndia (callback: (arg0: any) => void) {
-    const [err, response] = await to(
-        axios.default.get(indiaURl)
-    );
-    callback(response?.data?.statewise);
+async function getDetailsForIndia(callback: (arg0: any) => void) {
+  const [err, response] = await to(axios.default.get(indiaURl));
+  callback(response?.data?.statewise);
 }
 exports.getLatestData = getLatestData;
 exports.getDetailsForIndia = getDetailsForIndia;
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const clear = require('clear');
 const figlet = require('figlet');
 const program = require('commander');
 const Table = require('cli-table');
-const api =require('./api');
+const api = require('./api');
 var colWidth;
 
 clear();
@@ -13,102 +13,125 @@ console.log(
     figlet.textSync('Covid19', {
       font: 'Ghost',
       horizontalLayout: 'full',
-      verticalLayout: 'default'
+      verticalLayout: 'default',
     })
   )
 );
 
 function getCovidDetails(response: any) {
-  if(program.args[0] == 'india' && program.district) {
-
+  if (program.args[0] == 'india' && program.district) {
     let table = new Table({
-      head: ['S.No','State','Confirmed', 'Confirmed Today',chalk.red.bold('Deaths'),chalk.red.bold('Deaths Today'),chalk.green.bold('Recovered'),'Active Cases'],
-      colWidths: [5,25,15,20,15,15,15,15],
+      head: [
+        'S.No',
+        'State',
+        'Confirmed',
+        'Confirmed Today',
+        chalk.red.bold('Deaths'),
+        chalk.red.bold('Deaths Today'),
+        chalk.green.bold('Recovered'),
+        'Active Cases',
+      ],
+      colWidths: [5, 25, 15, 20, 15, 15, 15, 15],
       chars: {
-        'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
-        , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
-        , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
-        , 'right': '║', 'right-mid': '╢', 'middle': '│',
+        top: '═',
+        'top-mid': '╤',
+        'top-left': '╔',
+        'top-right': '╗',
+        bottom: '═',
+        'bottom-mid': '╧',
+        'bottom-left': '╚',
+        'bottom-right': '╝',
+        left: '║',
+        'left-mid': '╟',
+        mid: '─',
+        'mid-mid': '┼',
+        right: '║',
+        'right-mid': '╢',
+        middle: '│',
       },
     });
-    let totalCases =response[0];
+    let totalCases = response[0];
+    response.slice(0, 1);
     response.map((data: any, index: number) => {
       table.push([
         index + 1,
-        data.state,
-        data.confirmed,
-        data.delta.confirmed,
-        data.deaths,
-        data.delta.deaths,
-        data.recovered,
-        data.active
+        data?.state,
+        data?.confirmed,
+        data?.deltaconfirmed,
+        data?.deaths,
+        data?.deltadeaths,
+        data?.recovered,
+        data?.active,
       ]);
     });
     table.push([
       '',
-      totalCases.state,
-      totalCases.confirmed,
-      totalCases.delta.confirmed,
-      totalCases.deaths,
-      totalCases.delta.deaths,
-      totalCases.recovered,
-      totalCases.active
+      totalCases?.state,
+      totalCases?.confirmed,
+      totalCases?.deltaconfirmed,
+      totalCases?.deaths,
+      totalCases?.deltadeaths,
+      totalCases?.recovered,
+      totalCases?.active,
     ]);
     console.log(table.toString());
-
-  }
-
-  else {
+  } else {
     let table = new Table({
-      colWidths: [15,25],
+      colWidths: [35, 35],
       chars: {
-        'top': '═', 'top-mid': '╤', 'top-left': '╔', 'top-right': '╗'
-        , 'bottom': '═', 'bottom-mid': '╧', 'bottom-left': '╚', 'bottom-right': '╝'
-        , 'left': '║', 'left-mid': '╟', 'mid': '─', 'mid-mid': '┼'
-        , 'right': '║', 'right-mid': '╢', 'middle': '│',
+        top: '═',
+        'top-mid': '╤',
+        'top-left': '╔',
+        'top-right': '╗',
+        bottom: '═',
+        'bottom-mid': '╧',
+        'bottom-left': '╚',
+        'bottom-right': '╝',
+        left: '║',
+        'left-mid': '╟',
+        mid: '─',
+        'mid-mid': '┼',
+        right: '║',
+        'right-mid': '╢',
+        middle: '│',
       },
     });
 
-  var data = [];
+    var data = [];
 
- if(response == 'Country not found') {
+    if (response == 'Country not found') {
+      console.log('No matched data found');
+    } else {
+      for (let key in response) {
+        if (response.hasOwnProperty(key)) {
+          data.push([chalk.cyan.bold(key), chalk.green.bold(response[key])]);
+        }
+      }
+      table.push(...data);
 
-   console.log('No matched data found')
- }
-
- else {
-
-  for (let key in response) {
-
-    if (response.hasOwnProperty(key)) {
-
-      data.push([chalk.cyan.bold(key),chalk.green.bold(response[key])]
-
-    );
+      console.log(table.toString());
     }
   }
-  table.push(...data);
-
-  console.log(table.toString());
-}
-}
 }
 
 program
   .version('1.2.1')
-  .description("Track corona virus in your terminal")
-  .option('-d, --district', 'Get Get District wise data for India <cvirus india -d>')
+  .description('Track corona virus in your terminal')
+  .option(
+    '-d, --district',
+    'Get Get District wise data for India <cvirus india -d>'
+  )
   .option('-a, --all', 'Get Summary details')
   .parse(process.argv);
-if (!program.all  && program.args.length > 0 && !program.district) {
-  api.getCountryWiseData(program.args[0],getCovidDetails);
+if (!program.all && program.args.length > 0 && !program.district) {
+  api.getCountryWiseData(program.args[0], getCovidDetails);
 }
 
 if (program.all) {
-  api.getLatestData(getCovidDetails)
+  api.getLatestData(getCovidDetails);
 }
-if(program.args[0] == 'india' && program.district) {
-api.getDetailsForIndia(getCovidDetails);
+if (program.args[0] == 'india' && program.district) {
+  api.getDetailsForIndia(getCovidDetails);
 }
 
 if (!process.argv.slice(2).length) {
